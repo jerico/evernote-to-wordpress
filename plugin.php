6c496bae0d283e1683ebc18f4495c12b2b5a2237
Jerico Aragon <jerico@gmail.com>
2015-02-28 10:17:16 +0800

    Ability to update options in wp-admin

--- plugin.php
+++ plugin.php
@@ -9,12 +9,6 @@ Author URI: http://www.jericoaragon.com/
 */
 
 
-// Developer token. https://www.evernote.com/api/DeveloperToken.action
-$token = "";
-
-// Search notes with tag 'j'. https://dev.evernote.com/doc/articles/search_grammar.php
-$searchTerm = "tag:j";
-
 require('vendor/autoload.php');
 
 add_action('admin_menu', 'e2w_menu');
@@ -24,20 +18,59 @@ function e2w_menu() {
 }
 
 function e2w_page() {
+
+	$options = unserialize(get_option('e2w_options'));
+
 	echo '<div class="wrap">';
 	echo '<a href="admin.php?page=e2w&action=fetch" class="button">Fetch notes</a>';
+	echo '
+		<h2>Settings</h2>
+		<form method="POST" action="admin.php?page=e2w">
+			<table class="form-table">
+				<tr>
+					<th><label for="developerToken">Developer Token</label></th>
+					<td>
+						<input type="text" name="developerToken" style="width:25em" value="' . $options['developerToken'] . '"/>
+						<p class="description"><a href="https://www.evernote.com/api/DeveloperToken.action" target="_blank">Get yours here</a></p>
+					</td>
+				</tr>
+				<tr>
+					<th><label for="searchTerm">Search Term.</label></th>
+					<td>
+						<input type="text" name="searchTerm" style="width:25em" value="' . $options['searchTerm'] . '"/>
+						<p class="description"><a href="https://dev.evernote.com/doc/articles/search_grammar.php" target="_blank">Evernote Search Grammar</a></p>					</td>
+					</td>
+				</tr>
+			</table>
+			<p class="submit"><input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes"></p>
+		</form>
+	';
 	echo '</div>';
 
 	if (isset($_GET['action']) && $_GET['action'] == 'fetch') {
 		e2w_fetch_notes();
 		echo "<p>Imported!</p>";
 	}
+
+	if (isset($_POST['developerToken']) && $_POST['searchTerm']) {
+		$options = array(
+			'developerToken' => $_POST['developerToken'],
+			'searchTerm' => $_POST['searchTerm']
+		);
+
+		update_option('e2w_options', serialize($options));
+	}
 }
 
 function e2w_fetch_notes() {
 
-	global $token, $searchTerm;
-	$client = new \Evernote\Client($token, false);
+	$options = unserialize(get_option('e2w_options'));
+
+	$token = $options['developerToken'];
+	$searchTerm = $options['searchTerm'];
+	$sandbox = true;
+
+	$client = new \Evernote\Client($token, $sandbox);
 
 	$search = new \Evernote\Model\Search($searchTerm);
 
